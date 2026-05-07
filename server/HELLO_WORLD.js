0ac2ae2afa02616c07276d2e5ed50e9d8ac03137
thrinrthra <thinethrareddykv12345@gmail.com>
2026-05-07 21:09:29 +0530

    fixing AI service 6th time

---
+++ server/HELLO_WORLD.js
@@ -0,0 +1,4 @@
+console.log('****************************************');
+console.log('✅ I AM IN THE RIGHT FOLDER!');
+console.log('Current Directory:', __dirname);
+console.log('****************************************');
